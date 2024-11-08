2fc5f3c3679d187ab7df3c311ccd51d10277c83f
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-11-07 19:39:09 -0500

    added header, footer, and AnimesPage

---
+++ src/AnimesPage.jsx
@@ -0,0 +1,7 @@
+export function AnimesPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Create by Felix Gonzales</p>
+      <p>Copyright 2024</p>
+    </footer>
+  )
+}
