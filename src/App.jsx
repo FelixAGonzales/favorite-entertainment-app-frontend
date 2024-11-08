f80cb74e7edaba11b1b1c0611dbdb99b145a9be1
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-11-07 19:43:49 -0500

    added authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 import { AnimesPage } from "./AnimesPage";
 import { Footer } from "./Footer";
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <AnimesPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <AnimesPage />
-      <Footer />
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
